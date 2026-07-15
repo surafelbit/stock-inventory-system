05f437cedff0b8d42e645270df45b76ef35d5029
Your Real Name <surafelmuhabaw17@gmail.com>
2026-07-15 16:26:40 +0300

    making supplier model

--- app/Models/Supplier.php
+++ app/Models/Supplier.php
@@ -2,9 +2,17 @@
 
 namespace App\Models;
 
+use App\Traits\BelongsToCompany;
 use Illuminate\Database\Eloquent\Model;
 
 class Supplier extends Model
 {
-    //
+    use BelongsToCompany; // Instantly secures all supplier queries to the active tenant!
+
+    protected $fillable = [
+        'name',
+        'email',
+        'phone',
+        'address',
+    ];
 }
