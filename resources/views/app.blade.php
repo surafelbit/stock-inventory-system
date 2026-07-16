eef69f8ee7a489ba2e087a8cd9a6bff34c368988
Your Real Name <surafelmuhabaw17@gmail.com>
2026-07-16 10:48:40 +0300

    adding a beautiful welcome page

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -9,6 +9,9 @@
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@400;500;600;700;800;900&family=Inter:wght@400;500;600&family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&display=swap" rel="stylesheet" />
 
         <!-- Scripts -->
         @routes
