a49e86da12306d37d78e60e74d184ba49bf45249
Your Real Name <surafelmuhabaw17@gmail.com>
2026-07-22 16:59:28 +0300

    making the purchase table

--- database/migrations/2026_07_22_135513_create_purchases_table.php
+++ database/migrations/2026_07_22_135513_create_purchases_table.php
@@ -4,22 +4,25 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
+return new class extends Migration {
     public function up(): void
     {
         Schema::create('purchases', function (Blueprint $table) {
             $table->id();
+            // Multi-tenancy link
+            $table->foreignId('company_id')->constrained()->onDelete('cascade');
+            
+            // Foreign keys linking to Supplier and Product
+            $table->foreignId('supplier_id')->constrained()->onDelete('cascade');
+            $table->foreignId('product_id')->constrained()->onDelete('cascade');
+            
+            $table->integer('quantity');
+            $table->decimal('cost_price', 10, 2);
+            $table->decimal('total_price', 10, 2);
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('purchases');
