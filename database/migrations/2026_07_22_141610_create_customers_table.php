f4ac89d24a660b98434a580ff484c64d3d3b89f8
Your Real Name <surafelmuhabaw17@gmail.com>
2026-07-22 17:28:46 +0300

    making the customer table

--- database/migrations/2026_07_22_141610_create_customers_table.php
+++ database/migrations/2026_07_22_141610_create_customers_table.php
@@ -4,22 +4,22 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
+return new class extends Migration {
     public function up(): void
     {
         Schema::create('customers', function (Blueprint $table) {
             $table->id();
+            // Multi-tenancy link
+            $table->foreignId('company_id')->constrained()->onDelete('cascade');
+            
+            $table->string('name');
+            $table->string('email')->nullable();
+            $table->string('phone')->nullable();
+            $table->text('address')->nullable();
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('customers');
