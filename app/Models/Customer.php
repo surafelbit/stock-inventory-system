ffe7d321e099420924b894516d804ac2d171e181
Your Real Name <surafelmuhabaw17@gmail.com>
2026-07-22 17:30:52 +0300

    making the customer model

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -2,9 +2,17 @@
 
 namespace App\Models;
 
+use App\Traits\BelongsToCompany;
 use Illuminate\Database\Eloquent\Model;
 
 class Customer extends Model
 {
-    //
+    use BelongsToCompany; // Secures multi-tenant data isolation automatically!
+
+    protected $fillable = [
+        'name',
+        'email',
+        'phone',
+        'address',
+    ];
 }
